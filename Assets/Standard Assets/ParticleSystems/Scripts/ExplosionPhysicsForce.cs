9fc1dc0dc5799abcf0dde05b99c1812f35fa7555
Tim0theus <beckmann.timm@web.de>
2016-12-10 06:39:43 +0100

    repaired bomb

--- Assets/Standard Assets/ParticleSystems/Scripts/ExplosionPhysicsForce.cs
+++ Assets/Standard Assets/ParticleSystems/Scripts/ExplosionPhysicsForce.cs
@@ -32,6 +32,8 @@ namespace UnityStandardAssets.Effects
             {
                 rb.AddExplosionForce(explosionForce*multiplier, transform.position, r, 1*multiplier, ForceMode.Impulse);
             }
+
+            Destroy(gameObject);
         }
     }
 }
