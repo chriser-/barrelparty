b68a976fb4da8821d80be83e441c3a582396efcd
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-12-10 03:31:33 +0100

    Added Ventilator object

--- Assets/Scripts/Objects/ObjectVentilator.cs
+++ Assets/Scripts/Objects/ObjectVentilator.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ObjectVentilator : ObjectBase
 {
-	void Start ()
-    {
-		
-	}
+    public float m_strengthMax = 100.0f;
+    public float m_duration = 5.0f;
 
-	void Update ()
+    private void Start()
     {
-		
-	}
+        WindzoneController windController = GetComponentInChildren<WindzoneController>();
+        windController.strengthMax = m_strengthMax;
+        Destroy(windController, m_duration);
+    }
 }
