9ad8cabae8d101407f4d93bd7de05519763a2160
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-12-10 14:56:02 +0100

    Added wind particles for ventilator

--- Assets/Scripts/Items/ItemVentilator.cs
+++ Assets/Scripts/Items/ItemVentilator.cs
@@ -7,7 +7,7 @@ public class ItemVentilator : ItemBase
     protected override IEnumerator useItem()
     {
         ObjectVentilator ventilator = m_SpawnObject.PlaceObject<ObjectVentilator>(m_Player.transform.position);
-        ventilator.transform.Rotate(Vector3.up, 90.0f);
+        //ventilator.transform.Rotate(Vector3.up, 90.0f);
         ventilator.transform.position += new Vector3(0, m_Player.transform.localScale.y, 0) - m_Player.transform.forward * 0.25f;
         ventilator.gameObject.transform.SetParent(m_Player.transform);
         yield return null;
