f98007279187c0a5370ca4640d6f6e4f3e070701
yixuanL <anthea2323@sina.com>
2016-12-10 18:23:27 +0100

    b

--- Assets/Scripts/scrollingBackground.cs
+++ Assets/Scripts/scrollingBackground.cs
@@ -31,17 +31,23 @@ public class scrollingBackground : MonoBehaviour {
 	private void moveToLeft(){
 		Vector3 positionTmp = backgrounds [0].position;
 		float amtToMove = getSpeed() * Time.deltaTime;
-		transform.Translate(Vector3.left * amtToMove, Space.World);
-		if (backgrounds [2].position.x <= positionTmp.x)
-			backgrounds [2].position = backgrounds [0].position;
+		transform.Translate(Vector3.right * amtToMove, Space.World);
+		if (backgrounds [2].position.x <= positionTmp.x){
+			backgrounds[0].position = backgrounds[1].position;
+			backgrounds[1].position = backgrounds[2].position;
+			backgrounds [2].position = positionTmp;
+		}
 	}
 
 	private void moveToRight(){
 		Vector3 positionTmp = backgrounds [2].position;
 		float amtToMove = getSpeed() * Time.deltaTime;
 		transform.Translate(Vector3.right * amtToMove, Space.World);
-		if (backgrounds [0].position.x >= positionTmp.x)
-			backgrounds [0].position = backgrounds [2].position;
+		if (backgrounds [0].position.x >= positionTmp.x) {
+			backgrounds[2].position = backgrounds[1].position;
+			backgrounds[1].position = backgrounds[0].position;
+			backgrounds [0].position = positionTmp;
+		}
 	}
 
 
