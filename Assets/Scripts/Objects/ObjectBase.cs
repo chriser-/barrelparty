64e57a7f8c0d78d37dd502c5f02ca0ebac03ddd9
Christoph Winter <chriser@gmx.de>
2016-12-10 01:56:14 +0100

    added objects

---
+++ Assets/Scripts/Objects/ObjectBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectBase : MonoBehaviour
+{
+    public ObjectBase PlaceObject(Vector3 position)
+    {
+        ObjectBase newObjectBase = Instantiate(this);
+        newObjectBase.transform.position = position;
+        return newObjectBase;
+    }
+}
