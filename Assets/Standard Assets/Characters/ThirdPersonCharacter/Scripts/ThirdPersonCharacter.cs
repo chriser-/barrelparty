90d3003df89735fbb17e2007bdd3859e44b5d6ae
Egor Shevdin <egor_sh@live.de>
2016-12-10 18:56:09 +0100

    Trampolin

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -29,6 +29,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		Vector3 m_CapsuleCenter;
 		CapsuleCollider m_Capsule;
 		bool m_Crouching;
+	    private float m_InitJumpPower;
+
+	    public float JumpPower
+	    {
+	        get { return m_JumpPower; }
+	        set { m_JumpPower = Mathf.Max(0f, value); }
+	    }
 
 	    void Awake()
 	    {
@@ -44,6 +51,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 			m_OrigGroundCheckDistance = m_GroundCheckDistance;
+		    m_InitJumpPower = m_JumpPower;
 		}
 
 
@@ -75,8 +83,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			UpdateAnimator(move);
 		}
 
+	    public void ResetJumpPower()
+	    {
+	        m_JumpPower = m_InitJumpPower;
+	    }
 
-		void ScaleCapsuleForCrouching(bool crouch)
+
+	    void ScaleCapsuleForCrouching(bool crouch)
 		{
 			if (m_IsGrounded && crouch)
 			{
