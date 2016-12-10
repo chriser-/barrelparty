3fcbf3be1d65a810430b6200bfe99510aaac5a9f
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-12-10 23:33:33 +0100

    small glue script changes

--- Assets/Scripts/Items/ItemGlue.cs
+++ Assets/Scripts/Items/ItemGlue.cs
@@ -7,7 +7,8 @@ public class ItemGlue : ItemBase
 {
     protected override IEnumerator useItem()
     {
-        ObjectGlue glue = m_SpawnObject.PlaceObject<ObjectGlue>(m_Player.transform.position - m_Player.transform.forward * m_Player.transform.lossyScale.y * 1.5f);
+        ObjectGlue glue = m_SpawnObject.PlaceObject<ObjectGlue>(m_Player.transform.position - m_Player.transform.forward * m_Player.transform.lossyScale.y * 2.0f);
+        glue.transform.position += new Vector3(0, 0.01f, 0);
         //Ray ray = new Ray(glue.transform.position, Vector3.down);
         //RaycastHit hit;
         //if (Physics.Raycast(ray, out hit))
