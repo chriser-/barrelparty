7fbeb1cb9e4038c66983c985fe7e11af3ecdc9eb
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-12-10 13:06:17 +0100

    Empty item classes

---
+++ Assets/Scripts/Items/ItemGlue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGlue : ItemBase
+{
+    public override IEnumerator UseItem()
+    {
+        yield return null;
+    }
+}
