f0a27b29338869a9b52de25ca15ac00678b94b3f
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-12-11 03:23:40 +0100

    tests to come later

---
+++ Assets/Scripts/BackgroundRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRotation : MonoBehaviour
+{
+    public GameObject m_cameraObject;
+    [Range(0.1f, 1.0f)]
+    public float m_influenceFactor = 0.5f;
+
+    // Camera will rotate from -40 to 40
+    // We will know where the end point is
+    // ergo we can interpolate with influence parameter
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+	void Start ()
+    {
+        startPos = transform.position;
+        endPos = new Vector3(transform.position.x, 2.0f * transform.position.y * m_influenceFactor, transform.position.z);
+    }
+	
+	void Update ()
+    {
+        transform.position = Vector3.Lerp(startPos, endPos, (m_cameraObject.transform.rotation.x + 40.0f) / 80.0f);
+	}
+}
